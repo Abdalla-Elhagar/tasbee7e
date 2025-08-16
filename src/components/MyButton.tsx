772509871d1273b4a13e5fed40b2d90cc7088505
Abdalla Elhagar <nanoelhgar@gmail.com>
2025-08-16 10:28:54 +0300

    Refactor MyButton component to use button element instead of div for better accessibility

--- src/components/MyButton.tsx
+++ src/components/MyButton.tsx
@@ -4,13 +4,13 @@ import React, { type ReactNode } from "react";
 interface myButtonTypes {
     text: string | ReactNode;
     styles?: string;
-    onClick?: React.MouseEventHandler<HTMLDivElement>;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
     myRef?: any;
 }
 
 
 export default function MyButton({text, styles, onClick, myRef}:myButtonTypes) {
   return (
-    <div ref={myRef} onClick={onClick} className={`${styles} flex justify-center items-center cursor-pointer duration-300`}>{text}</div>
+    <button ref={myRef} onClick={onClick} className={`${styles} flex justify-center items-center cursor-pointer duration-300`}>{text}</button>
   )
 }
