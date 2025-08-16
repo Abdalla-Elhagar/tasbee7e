9cbba02149548588687f1b838c47b3d1e3640ed9
Abdalla Elhagar <nanoelhgar@gmail.com>
2025-08-16 16:09:19 +0300

    - added back button - added " إرجع من الاول " button to reset count

--- src/pages/Tasbee7Page.tsx
+++ src/pages/Tasbee7Page.tsx
@@ -2,7 +2,9 @@ import { useContext, useEffect, useState } from "react";
 import { azkarIdContext } from "../context/azkarIdContext";
 import type { zekrTypes } from "../components/AddZekrMenu";
 import MyButton from "../components/MyButton";
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { FaArrowRightLong } from "react-icons/fa6";
+
 
 export default function Tasbee7Page() {
   const navigate = useNavigate();
@@ -31,6 +33,20 @@ const [count, setCount] = useState<number>(zekr?.count || 0);
   });
 
   }
+
+  function handleReset() {
+    setCount(() => {
+    const newCount = 0;
+
+    setZekrData((data) =>
+      data.map((ele) =>
+        ele.id === id ? { ...ele, count: newCount } : ele
+      )
+    );
+
+    return newCount;
+  });
+  }
   
   useEffect(() => {
   if (id === undefined) return;
@@ -44,15 +60,19 @@ const [count, setCount] = useState<number>(zekr?.count || 0);
   }, [zekrData]);
   return (
     <main className="bg-black flex justify-center items-center h-screen">
-      <div className="container mx-auto max-w-full md:max-w-[600px] h-screen md:h-[600px] bg-neutral-800 flex flex-col items-center justify-between p-4 pt-6 gap-4">
+      <div className="container rounded-2xl mx-auto max-w-full md:max-w-[600px] h-screen md:h-[600px] bg-neutral-800 flex flex-col items-center justify-between p-4 pt-6 gap-4 relative">
+        <Link className="absolute top-5 right-5 text-2xl" to="/"><FaArrowRightLong /></Link>
         <div className="name text-2xl font-semibold">{zekr?.name}</div>
         <div
           style={{ whiteSpace: "pre-line", marginBottom: "10px" }}
-          className="desc text-lg font-semibold -mt-20"
+          className="desc text-lg font-semibold"
         >
           {zekr?.desc}
         </div>
         <div className="count text-[60px] font-bold mb-20">{count}</div>
+        <MyButton onClick={handleReset}
+          styles="bg-black py-2 px-4 rounded-lg text-xl active:scale-95 -mb-10"
+          text="إرجع من الأول" />
         <MyButton
           onClick={handleTasbee7}
           styles="bg-black w-full py-6 text-3xl active:scale-95"
